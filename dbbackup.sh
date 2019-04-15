f76a0436a3f0a0d125367be19e639479790f9d0d
Nel Ajoc <ajocr.garena@gmail.com>
2019-04-15 11:19:38 +0800

    modified for sample

--- dbbackup.sh
+++ dbbackup.sh
@@ -9,7 +9,7 @@ DB_BACKUP_PATH='/tmp/fromscripts/mysqlbackup'
 MYSQL_HOST='127.0.0.1'
 MYSQL_PORT='3306'
 MYSQL_USER='root'
-MYSQL_PASSWORD='perconapw'
+MYSQL_PASSWORD='password'
 #DATABASE_NAME=''
 #BACKUP_RETAIN_DAYS=30   ## Number of days to keep local backup copy
 
@@ -30,4 +30,4 @@ fi
 
 ### upload backup file to s3 bucket and delete the file on local machine
 
-aws s3 cp ${DB_BACKUP_PATH}/alldb-${TODAY}.sql.gz s3://sw-main-backup/w88/w88blog/ && rm  ${DB_BACKUP_PATH}/alldb-${TODAY}.sql.gz
+aws s3 cp ${DB_BACKUP_PATH}/alldb-${TODAY}.sql.gz s3://path/to/ && rm  ${DB_BACKUP_PATH}/alldb-${TODAY}.sql.gz
